6ae8b0e4496a1e0a20135e97611006851ce5de35
dev-hasan25 <devhasanali25@gmail.com>
2024-02-26 01:11:02 +0200

    create-component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-function App() {
-  return (
-    <>
-    <h1  className="App-title">Welcome to React</h1>
-    </>
-  );
+import Header from "./Header";
+
+function App()
+{
+	return (
+		<>
+			<Header  />
+		</>
+	);
 }
 
 export default App;
---
+++ src/Header.jsx
@@ -0,0 +1,11 @@
+
+export default function Header()
+{
+    return (
+        <header className="header">
+            <h1 className="App-title">Welcome to React</h1>
+            <h1 className="App-title">Welcome to React</h1>
+            <h1 className="App-title">Welcome to React</h1>
+        </header> 
+    );
+}
